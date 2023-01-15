5315a8f30981a25a389116780f9a9335664dc3eb
aakj2010 <aakj2010@gmail.com>
2023-01-15 19:33:05 +0530

    search

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,37 +1,37 @@
 class APIFeatures {
-    constructor(query, queryStr) {
+    constructor(query, queryStr){
         this.query = query;
         this.queryStr = queryStr;
     }
 
-    search() {
-        let keyword = this.queryStr.keyword ? {
+    search(){
+       let keyword =  this.queryStr.keyword ? {
             name: {
                 $regex: this.queryStr.keyword,
                 $options: 'i'
             }
-        } : {};
+       }: {};
 
-        this.query.find({ ...keyword });
-        return this;
+       this.query.find({...keyword})
+       return this;
     }
 
-    filter() {
-        const queryStrCopy = { ...this.queryStr };
 
+    filter(){
+        const queryStrCopy = { ...this.queryStr };
+  
         //removing fields from query
         const removeFields = ['keyword', 'limit', 'page'];
-        removeFields.forEach(field => delete queryStrCopy[field]);
-
+        removeFields.forEach( field => delete queryStrCopy[field]);
+        
         let queryStr = JSON.stringify(queryStrCopy);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)/g, match => `$${match}`)
-
+        queryStr =  queryStr.replace(/\b(gt|gte|lt|lte)/g, match => `$${match}`)
         this.query.find(JSON.parse(queryStr));
 
         return this;
     }
 
-    paginate(resPerPage) {
+    paginate(resPerPage){
         const currentPage = Number(this.queryStr.page) || 1;
         const skip = resPerPage * (currentPage - 1)
         this.query.limit(resPerPage).skip(skip);
@@ -39,5 +39,4 @@ class APIFeatures {
     }
 }
 
-
-module.exports = APIFeatures
+module.exports = APIFeatures;
