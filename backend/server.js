50deb0cf1148f9e4c6001ec8f47c026a45652fa8
aakj2010 <aakj2010@gmail.com>
2023-01-15 11:36:12 +0530

    env

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 const path = require("path");
 const connectDatabase = require('./config/database');
 
-dotenv.config({ path: path.join(__dirname, "config/config.env") })
+dotenv.config({ path: path.join(__dirname, "config/.env") })
 connectDatabase()
 
 const port = process.env.PORT || 5000
